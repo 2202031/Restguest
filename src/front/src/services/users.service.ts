02f1c451e1cda45a7aba19c716a288f6506f4a80
2202031 <2202031@utrivieramaya.edu.mx>
2026-04-19 14:07:20 -0500

    Fix invalid template literal in users service

--- src/front/src/services/users.service.ts
+++ src/front/src/services/users.service.ts
@@ -2,5 +2,5 @@ import { http } from './http';
 
 export const usersService = {
     getAll: () => http('/api/users', { method: 'GET' }),
-    getById: (id: string) => http(\`/api/users/\${id}\`, { method: 'GET' }),
+    getById: (id: string) => http(`/api/users/${id}`, { method: 'GET' }),
 };
